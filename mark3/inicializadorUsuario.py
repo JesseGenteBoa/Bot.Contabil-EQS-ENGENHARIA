9fb57db2284a69c6d5e096dad4110aef1c9d5677
JesseGenteBoa <153686378+JesseGenteBoa@users.noreply.github.com>
2024-06-10 13:51:26 -0300

    Update inicializadorUsuario.py

--- mark3/inicializadorUsuario.py
+++ mark3/inicializadorUsuario.py
@@ -10,7 +10,7 @@ from selenium.webdriver.chrome.service import Service
 FAILSAFE = True           
 
 
-ver_documento = r'C:\Users\User\OneDrive - EQS Engenharia Ltda\Documentos\GitHub\GitHubDoJessezinho\mark3\Imagens\verDocumentos.png'
+ver_documento = r'C:\Users\Usuario\Desktop\mark4\Imagens\verDocumentos.png'
 utils.insistirNoClique(ver_documento, cliques=1)
 time.sleep(0.4)
 insistir_no_clique = utils.encontrarImagem(ver_documento)
@@ -19,18 +19,18 @@ if type(insistir_no_clique) == pyscreeze.Box:
         utils.insistirNoClique(ver_documento, cliques=1)
         insistir_no_clique = utils.encontrarImagem(ver_documento)
         if type(insistir_no_clique) != pyscreeze.Box:
-            break        
+            break          
 hotkey("alt", "d", interval=0.1)
 time.sleep(0.5)
 hotkey("ctrl", "c")
 time.sleep(0.5)      
 link = pyperclip.paste()
 options = webdriver.ChromeOptions()
-options.add_argument(r'user-data-dir=C:\Users\User\AppData\Local\Google\Chrome\User Data\Perfil Selenium')
+options.add_argument(r'user-data-dir=C:\Users\Usuario\AppData\Local\Google\Chrome\User Data\Profile Selenium')
 servico = Service(ChromeDriverManager().install())
 driver = webdriver.Chrome(service=servico, options=options)
 driver.get(link)
-        
+    
     
 time.sleep(2)
 press(["tab"]*3)
@@ -38,4 +38,8 @@ write("bot.contabil")
 press("tab")
 write("EQSeng852@")
 press("enter")
-time.sleep(30)
+time.sleep(2)
+hotkey("alt", "tab", interval=0.1)
+hotkey("ctrl", "w")
+hotkey("alt", "tab", interval=0.1)
+time.sleep(15)
