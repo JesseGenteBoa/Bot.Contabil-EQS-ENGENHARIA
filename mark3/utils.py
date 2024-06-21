d94afc7162d085512d8797afcc7d4a5126c31e85
JesseGenteBoa <153686378+JesseGenteBoa@users.noreply.github.com>
2024-06-21 11:05:10 -0300

    Update utils.py

--- mark3/utils.py
+++ mark3/utils.py
@@ -1,36 +1,51 @@
-from pyautogui import locateOnScreen, locateCenterOnScreen, hotkey, press, write
+from pyautogui import locateOnScreen, locateCenterOnScreen, hotkey, press, position, write, FailSafeException
 from pydirectinput import click as mouseClique, moveTo
+from selenium import webdriver                         
+from pyperclip import paste
+from time import sleep
 import pyscreeze
-import pyperclip
-import time
+
+
+
+def checarFailsafe():
+    z, f = position()
+    if z == 0 and f == 0:
+        raise FailSafeException
+    
 
 def encontrarImagem(imagem):
     cont = 0
     while True:
         try:
             encontrou = locateOnScreen(imagem, grayscale=True, confidence = 0.8)
+            checarFailsafe()
             return encontrou
         except:
-            time.sleep(0.8)
+            sleep(0.8)
             cont += 1
             if cont == 3:
                 break
             print("Imagem não encontrada")
+            checarFailsafe()
             pass
+            
 
 def encontrarImagemLocalizada(imagem):
     cont = 0
     while True:
         try:
             x, y = locateCenterOnScreen(imagem, grayscale=True, confidence=0.92)
+            checarFailsafe()      
             return (x, y)
         except:
-            time.sleep(0.8)
+            sleep(0.8)
             cont += 1
             if cont == 3:
                 break
             print("Imagem não encontrada")
+            checarFailsafe()
             pass
+
 
 def formatador(variavel, casas_decimais="{:.2f}"):
     variavel = float(variavel)
@@ -56,99 +71,129 @@ def formatador4(variavel):
 def descerECopiar():
     press("down", interval=0.1)
     hotkey("ctrl", "c", interval=0.1)
+    checarFailsafe()
 
-def clicarMicrosiga(imagem=r'C:\Users\User\OneDrive - EQS Engenharia Ltda\Documentos\GitHub\GitHubDoJessezinho\mark3\Imagens\microsiga.png'):
+def clicarMicrosiga(imagem=r'C:\Users\Usuario\Desktop\mark4\Imagens\microsiga.png'):
     x, y = encontrarImagemLocalizada(imagem)
     mouseClique(x, y)
+    checarFailsafe()
+
+def mudarSelecao():
+    mudar_a_selecao = encontrarImagemLocalizada(imagem=r'C:\Users\Usuario\Desktop\mark4\Imagens\mudarASelecao.png')
+    x, y = mudar_a_selecao
+    mouseClique(x,y, clicks=4, interval=0.4)
+    sleep(1)
+    checarFailsafe()
 
 def voltarEDescer(passos=1):
     hotkey(["shift", "tab"]*passos, interval=0.15)
     press("down")
+    checarFailsafe()
 
 def reiniciarPortal():
     clicarMicrosiga()
     voltarEDescer(passos=3)
+    checarFailsafe()
 
 def cancelar1():
-    time.sleep(0.8)
+    sleep(0.8)
     voltarEDescer()
-    time.sleep(0.5)
+    sleep(0.5)
     clicarMicrosiga()
+    checarFailsafe()
 
 def cancelar2():
-    time.sleep(0.5)
+    sleep(0.5)
     cancelarLancamento()
     voltarEDescer()
-    time.sleep(0.3)
+    sleep(0.3)
     clicarMicrosiga()
+    checarFailsafe()
+
+def erroNoPortal():
+    sleep(0.3)
+    hotkey("alt", "tab", interval=0.1)
+    hotkey("ctrl", "w")
+    sleep(0.2)
+    reiniciarPortal()
+    sleep(0.2)
+    checarFailsafe()
+
+def cancelarEMudar():
+    cancelarLancamento()
+    mudarSelecao()
+    checarFailsafe()
 
 def escreverNatureza(natureza):
     press("enter")
     write(natureza)
     press("enter")
     press("left")
-    
+    checarFailsafe()
+
 
 def insistirNoClique(imagem, cliques=2):
     while True:
         try:
             clicarMicrosiga()
-            time.sleep(1.5)
+            sleep(1.5)
+            checarFailsafe()
             try:
                 mouseClique(250, 150)
+                checarFailsafe()
                 elemento = encontrarImagemLocalizada(imagem)
                 a, b = elemento
-                time.sleep(0.5)
+                sleep(0.5)
                 mouseClique(a,b, clicks=cliques, interval=0.1)
-                time.sleep(0.5)
+                sleep(0.5)
                 break
             except:
-                time.sleep(0.3)
+                sleep(0.3)
         except:
             moveTo(100, 150)
-            time.sleep(0.3)
+            sleep(0.3)
+    checarFailsafe()
 
 
-def clicarDadosDaNota():
-    imagem = r'C:\Users\User\OneDrive - EQS Engenharia Ltda\Documentos\GitHub\GitHubDoJessezinho\mark3\Imagens\DadosDaNota.png'
-    encontrar = encontrarImagemLocalizada(imagem)
+def clicarDadosDaNota(): 
+    encontrar = encontrarImagemLocalizada(r'C:\Users\Usuario\Desktop\mark4\Imagens\DadosDaNota.png')
     if type(encontrar) != tuple:            
-        insistirNoClique(imagem)
-        time.sleep(0.5)
+        insistirNoClique(r'C:\Users\Usuario\Desktop\mark4\Imagens\DadosDaNota.png')
+        sleep(0.5)
+        checarFailsafe()
     else:
         x, y = encontrar
         mouseClique(x,y, clicks=2)
     try:
-        aparece_enter = encontrarImagem(r'C:\Users\User\OneDrive - EQS Engenharia Ltda\Documentos\GitHub\GitHubDoJessezinho\mark3\Imagens\NCMsegue.png')
+        aparece_enter = encontrarImagem(r'C:\Users\Usuario\Desktop\mark4\Imagens\NCMsegue.png')
         if type(aparece_enter) == pyscreeze.Box:
-            time.sleep(0.5)
+            sleep(0.5)
             press("enter")
+            checarFailsafe()
     finally:
-        write("408")     
+        write("408")
+    checarFailsafe()
 
 
 def cancelarLancamento():
-    cancelar_lancamento_click = encontrarImagemLocalizada(r'C:\Users\User\OneDrive - EQS Engenharia Ltda\Documentos\GitHub\GitHubDoJessezinho\mark3\Imagens\CancelarLancamento.png')
+    cancelar_lancamento_click = encontrarImagemLocalizada(r'C:\Users\Usuario\Desktop\mark4\Imagens\CancelarLancamento.png')
     x, y = cancelar_lancamento_click
-    time.sleep(0.5)
+    sleep(0.5)
     mouseClique(x,y, clicks=3, interval=0.1)
+    checarFailsafe()
     while True:
-        cancelar_lancamento_click = encontrarImagemLocalizada(r'C:\Users\User\OneDrive - EQS Engenharia Ltda\Documentos\GitHub\GitHubDoJessezinho\mark3\Imagens\salvarLancamento.png')
+        cancelar_lancamento_click = encontrarImagemLocalizada(r'C:\Users\Usuario\Desktop\mark4\Imagens\salvarLancamento.png')
         if type(cancelar_lancamento_click) == tuple:
             mouseClique(x,y, clicks=2, interval=0.1)
+            checarFailsafe()
         else:
             break
-    aguarde = encontrarImagem(r'C:\Users\User\OneDrive - EQS Engenharia Ltda\Documentos\GitHub\GitHubDoJessezinho\mark3\Imagens\Aguarde.png') 
+    aguarde = encontrarImagem(r'C:\Users\Usuario\Desktop\mark4\Imagens\Aguarde.png') 
     while type(aguarde) == pyscreeze.Box:
-        aguarde = encontrarImagem(r'C:\Users\User\OneDrive - EQS Engenharia Ltda\Documentos\GitHub\GitHubDoJessezinho\mark3\Imagens\Aguarde.png') 
-        time.sleep(1)
+        aguarde = encontrarImagem(r'C:\Users\Usuario\Desktop\mark4\Imagens\Aguarde.png') 
+        sleep(1)
+    checarFailsafe()
 
-
-def mudarSelecao():
-    mudar_a_selecao = encontrarImagemLocalizada(imagem=r'C:\Users\User\OneDrive - EQS Engenharia Ltda\Documentos\GitHub\GitHubDoJessezinho\mark3\Imagens\mudarASelecao.png')
-    x, y = mudar_a_selecao
-    mouseClique(x,y, clicks=4, interval=0.4)
-    time.sleep(1)
     
 
 def contarItemFracionado(quantidade_siga, valor_unit, quantidade_real):
@@ -157,26 +202,27 @@ def contarItemFracionado(quantidade_siga, valor_unit, quantidade_real):
     quantidade_total = []
     quantidade_total.append(quantidade_siga)
     press("left")
-    time.sleep(0.2)
+    sleep(0.2)
     hotkey("ctrl", "c", interval=0.5)
-    cod_item = pyperclip.paste()
+    cod_item = paste()
+    checarFailsafe()
     try:
         while sum(quantidade_total) < quantidade_real:
             press("down")
-            time.sleep(0.5)
+            sleep(0.5)
             hotkey("ctrl", "c", interval=0.8)
-            item_dividido = pyperclip.paste()
-            cont += 1
+            item_dividido = paste()
+            cont+=1
             if item_dividido == cod_item:
                 press(["right"]*6)
                 hotkey("ctrl", "c", interval=0.8)
-                qtd_dividida = pyperclip.paste()
-                qtd_dividida = formatador4
-                (qtd_dividida)
+                qtd_dividida = paste()
+                qtd_dividida = formatador4(qtd_dividida)
                 quantidade_total.append(qtd_dividida)
                 press("right")
                 write(valor_unit, interval=0.05)
                 press(["left"]*8)
+                checarFailsafe()        
             else:
                 break
     except TypeError:
@@ -185,38 +231,90 @@ def contarItemFracionado(quantidade_siga, valor_unit, quantidade_real):
         press(["up"]*cont, interval=20)
     else:
         press(["up"]*cont, interval=0.1)
-    time.sleep(0.5)
+    sleep(0.5)
     press(["right"]*7)
-    time.sleep(0.5)
+    sleep(0.5)
     write(valor_unit, interval=0.05)
+    checarFailsafe()
     return quantidade_total
-
-def cancelarEMudar():
-    cancelarLancamento()
-    mudarSelecao()
 
 
 def clicarValorParcela():
-    valor_parcela = encontrarImagemLocalizada(r'C:\Users\User\OneDrive - EQS Engenharia Ltda\Documentos\GitHub\GitHubDoJessezinho\mark3\Imagens\clicarParcela.png')
+    valor_parcela = encontrarImagemLocalizada(r'C:\Users\Usuario\Desktop\mark4\Imagens\clicarParcela.png')
     while type(valor_parcela) != tuple:
         moveTo(180, 200)
-        aba_duplicatas = encontrarImagemLocalizada(r'C:\Users\User\OneDrive - EQS Engenharia Ltda\Documentos\GitHub\GitHubDoJessezinho\mark3\Imagens\AbaDuplicatas.png')
+        aba_duplicatas = encontrarImagemLocalizada(r'C:\Users\Usuario\Desktop\mark4\Imagens\AbaDuplicatas.png')
         x, y =  aba_duplicatas
+        checarFailsafe()
         mouseClique(x,y, clicks=4, interval=0.1)
-        valor_parcela = encontrarImagemLocalizada(r'C:\Users\User\OneDrive - EQS Engenharia Ltda\Documentos\GitHub\GitHubDoJessezinho\mark3\Imagens\clicarParcela.png')
-        time.sleep(0.4)
+        valor_parcela = encontrarImagemLocalizada(r'C:\Users\Usuario\Desktop\mark4\Imagens\clicarParcela.png')
+        sleep(0.4)
     x, y = valor_parcela
     mouseClique(x,y)
+    checarFailsafe()
 
 
 def clicarNaturezaDuplicata():
     while True:
-        natureza_duplicata_clique = encontrarImagemLocalizada(r'C:\Users\User\OneDrive - EQS Engenharia Ltda\Documentos\GitHub\GitHubDoJessezinho\mark3\Imagens\naturezaDuplicata.png')
+        natureza_duplicata_clique = encontrarImagemLocalizada(r'C:\Users\Usuario\Desktop\mark4\Imagens\naturezaDuplicata.png')
+        checarFailsafe()
         if type(natureza_duplicata_clique) != tuple:
             moveTo(150, 250)
             mouseClique(x,y, clicks=4, interval=0.1)
-            time.sleep(0.3)
+            sleep(0.3)
         else:
             break
     x, y = natureza_duplicata_clique
     mouseClique(x,y)
+    checarFailsafe()
+
+
+def acrescerLista(lista, lista2, link):
+    try:
+        verificador = lista.index(link)
+    except:
+        lista.append(link)
+    try:
+        verificador = lista2.index(link)
+    except:
+        lista2.append(link)
+
+
+def tratarLista(lista1, lista2):
+    print(lista1, lista2)
+    lista_unica = lista1 + lista2
+    print(lista1)
+    lista_unica = list(set(lista_unica))
+    print(lista1)
+    return lista_unica
+
+
+def abrirLinkSelenium(lista):
+    options = webdriver.ChromeOptions()
+    options.add_argument(r'user-data-dir=C:\Users\Usuario\AppData\Local\Google\Chrome\User Data\Profile Selenium')
+    driver = webdriver.Chrome(options=options)
+    print(lista)
+    if len(lista) > 1:
+        try:
+            driver.get(lista[0])
+            for link in lista[1:]:
+                sleep(0.2)
+                driver.execute_script("window.open('');")
+                driver.switch_to.window(driver.window_handles[-1])
+                sleep(0.5)
+                driver.get(link)
+            while True:
+                sleep(1)
+                if not driver.window_handles:
+                    break
+        except IndexError:
+            pass
+    else: 
+        try:
+            driver.get(lista[0])
+            while True:
+                sleep(1)
+                if not driver.window_handles:
+                    break
+        except IndexError:
+            driver.quit()
