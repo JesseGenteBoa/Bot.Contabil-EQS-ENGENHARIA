ba1d95eb19f98a7cfd5c476755cffbf0bf542686
JesseGenteBoa <153686378+JesseGenteBoa@users.noreply.github.com>
2024-06-21 11:06:39 -0300

    Update operadoresLancamento.py

--- mark3/operadoresLancamento.py
+++ mark3/operadoresLancamento.py
@@ -1,46 +1,51 @@
-from pyautogui import *
-import pyperclip
+from pyautogui import hotkey, press, write, FailSafeException
+from pyperclip import paste
+from time import sleep
 import pyscreeze
-import time
 import utils
 
 
 def escreverValorUnit(valor_unit_convertido, passos=6):
     press(["right"]*passos)
     valor_unit_convertido = utils.formatador(valor_unit_convertido, casas_decimais="{:.6f}")
-    time.sleep(0.2)
+    sleep(0.2)
     write(valor_unit_convertido)
-    time.sleep(0.2)
+    sleep(0.2)
     press(["right"]*3)
+    utils.checarFailsafe()
  
  
 def verificarValorDoItem(lista, indiceX):
     razoes = []
-    time.sleep(0.7)
+    sleep(0.7)
     cancelar_lancamento = False
     press(["right"]*4)
-    time.sleep(0.7)
+    sleep(0.7)
     hotkey("ctrl", "c")
-    time.sleep(0.7)
-    valor_do_item_no_siga = pyperclip.paste()
+    sleep(0.7)
+    utils.checarFailsafe()
+    valor_do_item_no_siga = paste()
     valor_do_item_no_siga = utils.formatador4(valor_do_item_no_siga)
     valor_do_item_na_NF = lista[indiceX][0]
     valor_do_item_na_NF = utils.formatador3(valor_do_item_na_NF)
     if valor_do_item_no_siga != valor_do_item_na_NF:
         write(lista[indiceX][0])
-        time.sleep(0.8)
-        encontrar = utils.encontrarImagem(r'C:\Users\User\OneDrive - EQS Engenharia Ltda\Documentos\GitHub\GitHubDoJessezinho\mark3\Imagens\valitenErrado.png')
+        sleep(0.8)
+        encontrar = utils.encontrarImagem(r'C:\Users\Usuario\Desktop\mark4\Imagens\valitenErrado.png')
+        utils.checarFailsafe()
         if type(encontrar) == pyscreeze.Box:
             press("enter")
-            time.sleep(0.5)
-            encontrar = utils.encontrarImagem(r'C:\Users\User\OneDrive - EQS Engenharia Ltda\Documentos\GitHub\GitHubDoJessezinho\mark3\Imagens\valitenErrado.png')
+            sleep(0.5)
+            encontrar = utils.encontrarImagem(r'C:\Users\Usuario\Desktop\mark4\Imagens\valitenErrado.png')
+            utils.checarFailsafe()
             if type(encontrar) == pyscreeze.Box:
                 press("enter")
             press("esc")
             press(["left"]*5)
-            time.sleep(0.2)
+            sleep(0.2)
             hotkey("ctrl", "c", interval=0.5)
-            quantidade_siga = pyperclip.paste()
+            utils.checarFailsafe()
+            quantidade_siga = paste()
             quantidade_siga = utils.formatador4(quantidade_siga)
             quantidade_NF = lista[indiceX][1]
             quantidade_NF = utils.formatador3(quantidade_NF)
@@ -48,17 +53,20 @@ def verificarValorDoItem(lista, indiceX):
             valor_unit_NF = utils.formatador3(valor_unit_NF)
             if quantidade_siga == quantidade_NF:
                 escreverValorUnit(valor_unit_NF, passos=1)
+                utils.checarFailsafe()
             else:
                 press(["left"]*5)
-                time.sleep(0.2)
+                sleep(0.2)
                 hotkey("ctrl", "c", interval=0.5)
-                desc_prod = pyperclip.paste()
+                utils.checarFailsafe()
+                desc_prod = paste()
                 desc_prod = desc_prod.lower()
                 if "abracadeira" in desc_prod:
                     quantidade_convertida = quantidade_NF * 100
                     if quantidade_convertida == quantidade_siga:
                         valor_unit_convertido = valor_unit_NF / 100
                         escreverValorUnit(valor_unit_convertido)
+                        utils.checarFailsafe()
                     else:
                         quantidade_total = utils.contarItemFracionado(quantidade_siga, valor_unit_convertido, quantidade_convertida)
                         try:
@@ -70,14 +78,17 @@ def verificarValorDoItem(lista, indiceX):
                                     razao = qtd / quantidade_convertida
                                     razoes.append(razao)
                                 press(["right"]*3)
+                            utils.checarFailsafe()
                         except TypeError:
                             cancelar_lancamento = True
                             utils.cancelarEMudar()
+                            utils.checarFailsafe()
                 elif "pilha" in desc_prod:
                     quantidade_convertida = quantidade_NF * 2
                     if quantidade_convertida == quantidade_siga:
                         valor_unit_convertido = valor_unit_NF / 2
                         escreverValorUnit(valor_unit_convertido)
+                        utils.checarFailsafe()
                     else:
                         quantidade_total = utils.contarItemFracionado(quantidade_siga, valor_unit_convertido, quantidade_convertida)
                         try:
@@ -89,14 +100,17 @@ def verificarValorDoItem(lista, indiceX):
                                     razao = qtd / quantidade_convertida
                                     razoes.append(razao)
                                 press(["right"]*3)
+                            utils.checarFailsafe()
                         except TypeError:
                             cancelar_lancamento = True
                             utils.cancelarEMudar()
+                            utils.checarFailsafe()
                 elif "gas" in desc_prod:
                     press("left")
                     hotkey("ctrl", "c", interval=0.5)
-                    cod_do_item = pyperclip.paste()
+                    cod_do_item = paste()
                     press("right")
+                    utils.checarFailsafe()
                     if cod_do_item == "0651000053":
                         quantidade_total = utils.contarItemFracionado(quantidade_siga, valor_unit_NF, quantidade_NF)
                         try:
@@ -108,15 +122,19 @@ def verificarValorDoItem(lista, indiceX):
                                     razao = qtd / quantidade_NF
                                     razoes.append(razao)
                                 press(["right"]*3)
+                            utils.checarFailsafe()
                         except TypeError:
                             cancelar_lancamento = True
                             utils.cancelarEMudar()
+                            utils.checarFailsafe()
                     else:
                         valor_unit_convertido = valor_do_item_na_NF / quantidade_siga
                         escreverValorUnit(valor_unit_convertido)
+                        utils.checarFailsafe()
                 elif "pedrisco" in desc_prod or "cabo" in desc_prod or "manta" in desc_prod or "lona" in desc_prod:
                     valor_unit_convertido = valor_do_item_na_NF / quantidade_siga
                     escreverValorUnit(valor_unit_convertido)
+                    utils.checarFailsafe()
                 else:
                     quantidade_total = utils.contarItemFracionado(quantidade_siga, valor_unit_NF, quantidade_NF)
                     try:
@@ -128,19 +146,22 @@ def verificarValorDoItem(lista, indiceX):
                                 razao = qtd / quantidade_NF
                                 razoes.append(razao)
                             press(["right"]*3)
+                        utils.checarFailsafe()
                     except TypeError:
                         cancelar_lancamento = True
                         utils.cancelarEMudar()
+                        utils.checarFailsafe()
         else:
             press("left")
+        utils.checarFailsafe()
     return cancelar_lancamento, razoes
 
 
 def copiarNatureza():
     press("right", interval=0.7)
     hotkey("ctrl", "c")
-    time.sleep(0.7)
-    natureza = pyperclip.paste()
+    sleep(0.7)
+    natureza = paste()
     if natureza == "2020081":
         natureza = "2050006"
         utils.escreverNatureza(natureza)
@@ -150,7 +171,7 @@ def copiarNatureza():
     elif natureza in ["2020082", "2020083"]:
         natureza = "2050008"
         utils.escreverNatureza(natureza)
-    
+    utils.checarFailsafe()
     return natureza
 
 
@@ -202,7 +223,7 @@ def definirTES(codigo, ctrl_imposto):
             tes = "432"
     elif codigo == 5:
         hotkey("ctrl", "c", interval=0.5)
-        tes_padrao = pyperclip.paste()
+        tes_padrao = paste()
         if tes_padrao == "406":
             tes = "406"
         else:
@@ -216,14 +237,14 @@ def definirTES(codigo, ctrl_imposto):
                 tes = "403"
     elif codigo == 6:
         hotkey("ctrl", "c", interval=0.5)
-        tes_padrao = pyperclip.paste()
+        tes_padrao = paste()
         if tes_padrao == "406":
             tes = "406"
         else:
             press(["left"]*2)
-            time.sleep(0.7)
+            sleep(0.7)
             hotkey("ctrl", "c", interval=0.5)
-            item_especifico = pyperclip.paste()
+            item_especifico = paste()
             press(["right"]*2)
             if item_especifico in ["1312000156", "999920091200", "999949011000", "1303102887", "1302578", "1303100449", "1303100601", "1303100602", "1303100603", "1312000122", "1312000124", "1312000125", "1312000126", "1312000144", "1308002", "1312024", "1303100550", "1303100600", "1303101290", "1303101291", "1303103835", "1303103836", "1303103837", "1312000141"]:
                 if ctrl_imposto != 0:
@@ -243,9 +264,9 @@ def definirTES(codigo, ctrl_imposto):
         cancelar_lancamento = True
         utils.cancelarLancamento()
         utils.voltarEDescer()
-        time.sleep(0.3)
+        sleep(0.3)
         tes = cancelar_lancamento
-
+    utils.checarFailsafe()
     return tes
     
 
@@ -255,96 +276,110 @@ def zerarImposto(passos_ida=7, passos_volta=8):
     press("backspace")
     press("enter")
     press(["left"]*passos_volta)
+    utils.checarFailsafe()
 
 
 def escreverTES(tes):
     press("enter", interval=0.3)
     write(tes)
     press(["right"]*4)
+    utils.checarFailsafe()
 
 
 def inserirDesconto(desc_no_item):
     press(["right"]*3)
-    time.sleep(0.5)
+    sleep(0.5)
     press("enter")
     desc_no_item = utils.formatador2(desc_no_item)
     write(desc_no_item, interval=0.02)
-    time.sleep(0.5)
+    sleep(0.5)
+    utils.checarFailsafe()
 
 
 def inserirFrete(frete_no_item):
     press(["right"]*105)
-    time.sleep(0.6)
+    sleep(0.6)
     press("enter")
     frete_no_item = utils.formatador2(frete_no_item)
     write(frete_no_item, interval=0.05)
-    time.sleep(0.6)
+    sleep(0.6)
+    utils.checarFailsafe()
 
 
 def inserirSeguro(seg_no_item):
-    time.sleep(0.3)
+    sleep(0.3)
     press("enter")
     seg_no_item = utils.formatador2(seg_no_item)
     write(seg_no_item, interval=0.05)
-    time.sleep(0.6)
+    sleep(0.6)
+    utils.checarFailsafe()
 
 
 def inserirDespesa(desp_no_item):
-    time.sleep(0.3)
+    sleep(0.3)
     press("enter")
     desp_no_item = utils.formatador2(desp_no_item)
     write(desp_no_item, interval=0.05)
-    time.sleep(0.6)
+    sleep(0.6)
     press(["left"]*112)
+    utils.checarFailsafe()
 
 
 def inserirICMS(icms_no_item, bc_icms, aliq_icms):
     press(["right"]*7)
-    time.sleep(0.5)
+    sleep(0.5)
     press("enter")
     bc_icms = utils.formatador2(bc_icms)
     write(bc_icms)
     press(["right"]*8)
-    time.sleep(0.5)
+    sleep(0.5)
     press("enter")
+    utils.checarFailsafe()
     write(aliq_icms)
-    time.sleep(0.5)
+    sleep(0.5)
     press(["left"]*9)
-    time.sleep(0.5)
+    sleep(0.5)
     press("enter")
     icms_no_item = utils.formatador2(icms_no_item)
     write(icms_no_item)
+    utils.checarFailsafe()
 
 
 def inserirICMSST(icmsST_no_item, base_icms_ST, aliq_icms_ST, passosST=9):
     press(["right"]*passosST)
-    time.sleep(0.4)
+    sleep(0.5)
     press("enter")
     base_icms_ST = utils.formatador2(base_icms_ST)
     write(base_icms_ST)
-    time.sleep(0.4)
+    sleep(0.5)
     press("enter")
+    utils.checarFailsafe()
     write(aliq_icms_ST)
-    time.sleep(0.4)
+    sleep(0.5)
     press("enter")
     icmsST_no_item = utils.formatador2(icmsST_no_item)
     write(icmsST_no_item)
     press(["left"]*12)    
-    
+    utils.checarFailsafe()
+
 
 def inserirIPI(ipi_no_item, base_ipi, aliq_ipi, passosIPI=12):
     press(["right"]*passosIPI)
-    time.sleep(0.5)
+    sleep(0.5)
     press("enter")
     base_ipi = utils.formatador2(base_ipi)
     write(base_ipi)
     press(["right"]*5)
-    time.sleep(0.5)
+    sleep(0.5)
     press("enter")
+    utils.checarFailsafe()
     write(aliq_ipi)
     press(["left"]*6)
-    time.sleep(0.5)
+    sleep(0.5)
     press("enter")
     ipi_no_item = utils.formatador2(ipi_no_item)
     write(ipi_no_item)
     press(["left"]*14)
+    utils.checarFailsafe()
+
+    
