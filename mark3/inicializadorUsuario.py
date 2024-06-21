117ce2c571bc41dc8f4fc1ce74ed1076e102990c
JesseGenteBoa <153686378+JesseGenteBoa@users.noreply.github.com>
2024-06-21 11:07:09 -0300

    Update inicializadorUsuario.py

--- mark3/inicializadorUsuario.py
+++ mark3/inicializadorUsuario.py
@@ -1,45 +1,49 @@
-from pyautogui import *        
-import pyperclip
-import pyscreeze
-import time
-import utils
+from pyautogui import hotkey, press, write, FailSafeException
+from pyperclip import paste
+from time import sleep
 from selenium import webdriver                         
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
+import utils
+import pyscreeze
+         
 
-FAILSAFE = True           
-
-
-ver_documento = r'C:\Users\User\OneDrive - EQS Engenharia Ltda\Documentos\GitHub\GitHubDoJessezinho\mark3\Imagens\verDocumentos.png'
-utils.insistirNoClique(ver_documento, cliques=1)
-time.sleep(0.4)
-insistir_no_clique = utils.encontrarImagem(ver_documento)
-if type(insistir_no_clique) == pyscreeze.Box:
-    while True:
-        utils.insistirNoClique(ver_documento, cliques=1)
-        insistir_no_clique = utils.encontrarImagem(ver_documento)
-        if type(insistir_no_clique) != pyscreeze.Box:
-            break        
-hotkey("alt", "d", interval=0.1)
-time.sleep(0.5)
-hotkey("ctrl", "c")
-time.sleep(0.5)      
-link = pyperclip.paste()
-options = webdriver.ChromeOptions()
-options.add_argument(r'user-data-dir=C:\Users\User\AppData\Local\Google\Chrome\User Data\Perfil Selenium')
-servico = Service(ChromeDriverManager().install())
-driver = webdriver.Chrome(service=servico, options=options)
-driver.get(link)
+def inicializarUsuario():
+    ver_documento = r'C:\Users\Usuario\Desktop\mark4\Imagens\verDocumentos.png'
+    utils.insistirNoClique(ver_documento, cliques=1)
+    sleep(0.4)
+    insistir_no_clique = utils.encontrarImagem(ver_documento)
+    if type(insistir_no_clique) == pyscreeze.Box:
+        while True:
+            utils.insistirNoClique(ver_documento, cliques=1)
+            insistir_no_clique = utils.encontrarImagem(ver_documento)
+            if type(insistir_no_clique) != pyscreeze.Box:
+                break          
+    hotkey("alt", "d", interval=0.1)
+    sleep(0.5)
+    hotkey("ctrl", "c")
+    sleep(0.5)      
+    link = paste()
+    options = webdriver.ChromeOptions()
+    options.add_argument(r'user-data-dir=C:\Users\Usuario\AppData\Local\Google\Chrome\User Data\Profile Selenium')
+    servico = Service(ChromeDriverManager().install())
+    driver = webdriver.Chrome(service=servico, options=options)
+    driver.get(link)
+    utils.checarFailsafe()
         
-    
-time.sleep(2)
-press(["tab"]*3)
-write("bot.contabil")
-press("tab")
-write("EQSeng852@")
-press("enter")
-time.sleep(2)
-hotkey("alt", "tab", interval=0.1)
-hotkey("ctrl", "w")
-hotkey("alt", "tab", interval=0.1)
-time.sleep(10)
+        
+    sleep(2)
+    press(["tab"]*3)
+    write("bot.contabil")
+    press("tab")
+    write("EQSeng852@")
+    press("enter")
+    sleep(2)
+    hotkey("alt", "tab", interval=0.1)
+    hotkey("ctrl", "w")
+    hotkey("alt", "tab", interval=0.1)
+    sleep(7)
+    utils.checarFailsafe()
+
+
+
