8ca1fe6fd2ca8a5dc4a006026fd8d84d0de7d4ae
JesseGenteBoa <153686378+JesseGenteBoa@users.noreply.github.com>
2024-10-18 18:04:24 -0300

    Update utils.py

--- mark3/utils.py
+++ mark3/utils.py
@@ -8,20 +8,14 @@ import smtplib
 import pyscreeze
 
 
-FAILSAFE = False
-
-def checarFailsafe():
-    z, f = position()
-    if z == 0 and f == 0:
-        raise FailSafeException
+FAILSAFE = True
     
 
 def encontrarImagem(imagem):
     cont = 0
     while True:
         try:
-            encontrou = locateOnScreen(imagem, grayscale=True, confidence = 0.8)
-            checarFailsafe()
+            encontrou = locateOnScreen(imagem, grayscale=True, confidence = 0.77)
             return encontrou
         except:
             sleep(0.8)
@@ -29,7 +23,6 @@ def encontrarImagem(imagem):
             if cont == 3:
                 break
             print("Imagem não encontrada")
-            checarFailsafe()
             pass
             
 
@@ -37,8 +30,7 @@ def encontrarImagemLocalizada(imagem):
     cont = 0
     while True:
         try:
-            x, y = locateCenterOnScreen(imagem, grayscale=True, confidence=0.92)
-            checarFailsafe()      
+            x, y = locateCenterOnScreen(imagem, grayscale=True, confidence=0.92)     
             return (x, y)
         except:
             sleep(0.8)
@@ -46,7 +38,6 @@ def encontrarImagemLocalizada(imagem):
             if cont == 3:
                 break
             print("Imagem não encontrada")
-            checarFailsafe()
             pass
 
 
@@ -56,62 +47,73 @@ def formatador(variavel, casas_decimais="{:.2f}"):
     variavel = variavel.replace(".", ",")
     return variavel
 
+
 def formatador2(variavel):
     variavel = float(variavel)
     variavel = "{:.2f}".format(variavel)
     return variavel
+
 
 def formatador3(variavel):
     variavel = variavel.replace(",", ".")
     variavel = float(variavel)
     return variavel
 
+
 def formatador4(variavel):
     variavel = variavel.replace(".", "")
     variavel = formatador3(variavel)
     return variavel
 
+
 def descerECopiar():
     press("down", interval=0.1)
     hotkey("ctrl", "c", interval=0.1)
-    checarFailsafe()
 
-def clicarMicrosiga(imagem=r'_internal\Imagens\microsiga.png'):
+
+def clicarMicrosiga(imagem=r'Imagens\microsiga.png'):
     x, y = encontrarImagemLocalizada(imagem)
     mouseClique(x, y)
-    checarFailsafe()
+
 
 def mudarSelecao():
-    mudar_a_selecao = encontrarImagemLocalizada(imagem=r'_internal\Imagens\mudarASelecao.png')
+    mudar_a_selecao = encontrarImagemLocalizada(imagem=r'Imagens\mudarASelecao.png')
     x, y = mudar_a_selecao
     mouseClique(x,y, clicks=4, interval=0.4)
     sleep(1)
-    checarFailsafe()
+
 
 def voltarEDescer(passos=1):
     hotkey(["shift", "tab"]*passos, interval=0.15)
     press("down")
-    checarFailsafe()
+
 
 def reiniciarPortal():
     clicarMicrosiga()
     voltarEDescer(passos=3)
-    checarFailsafe()
+
 
 def cancelar1():
     sleep(0.8)
     voltarEDescer()
     sleep(0.5)
     clicarMicrosiga()
-    checarFailsafe()
+
 
 def cancelar2():
     sleep(0.5)
     cancelarLancamento()
-    voltarEDescer()
-    sleep(0.3)
-    clicarMicrosiga()
-    checarFailsafe()
+    cancelar1()
+
+
+def cancelar3():
+    sleep(0.5)
+    cancelarLancamento()
+    sleep(2)
+    press("esc", interval=1)
+    press("enter")
+    cancelar1()
+
 
 def erroNoPortal():
     sleep(0.3)
@@ -120,19 +122,18 @@ def erroNoPortal():
     sleep(0.2)
     reiniciarPortal()
     sleep(0.2)
-    checarFailsafe()
+
 
 def cancelarEMudar():
     cancelarLancamento()
     mudarSelecao()
-    checarFailsafe()
+
 
 def escreverNatureza(natureza):
     press("enter")
     write(natureza)
     press("enter")
     press("left")
-    checarFailsafe()
 
 
 def insistirNoClique(imagem, cliques=2):
@@ -140,13 +141,11 @@ def insistirNoClique(imagem, cliques=2):
         try:
             clicarMicrosiga()
             sleep(1.5)
-            checarFailsafe()
             try:
                 mouseClique(250, 150)
                 elemento = encontrarImagemLocalizada(imagem)
                 a, b = elemento
                 sleep(0.5)
-                checarFailsafe()
                 mouseClique(a,b, clicks=cliques, interval=0.1)
                 sleep(0.5)
                 break
@@ -155,47 +154,38 @@ def insistirNoClique(imagem, cliques=2):
         except:
             moveTo(100, 150)
             sleep(0.3)
-    checarFailsafe()
 
 
 def clicarDadosDaNota(): 
-    encontrar = encontrarImagemLocalizada(r'_internal\Imagens\DadosDaNota.png')
+    encontrar = encontrarImagemLocalizada(r'Imagens\DadosDaNota.png')
     if type(encontrar) != tuple:            
-        insistirNoClique(r'_internal\Imagens\DadosDaNota.png')
+        insistirNoClique(r'Imagens\DadosDaNota.png')
         sleep(0.5)
-        checarFailsafe()
     else:
         x, y = encontrar
         mouseClique(x,y, clicks=2)
     try:
-        aparece_enter = encontrarImagem(r'_internal\Imagens\NCMsegue.png')
+        aparece_enter = encontrarImagem(r'Imagens\NCMsegue.png')
         if type(aparece_enter) == pyscreeze.Box:
             sleep(0.5)
             press("enter")
-            checarFailsafe()
     finally:
         write("408")
-    checarFailsafe()
 
 
 def cancelarLancamento():
-    cancelar_lancamento_click = encontrarImagemLocalizada(r'_internal\Imagens\CancelarLancamento.png')
-    x, y = cancelar_lancamento_click
-    sleep(0.5)
-    mouseClique(x,y, clicks=3, interval=0.1)
-    checarFailsafe()
     while True:
-        cancelar_lancamento_click = encontrarImagemLocalizada(r'_internal\Imagens\salvarLancamento.png')
-        if type(cancelar_lancamento_click) == tuple:
-            mouseClique(x,y, clicks=2, interval=0.1)
-            checarFailsafe()
-        else:
+        cancelar_lancamento_click = encontrarImagemLocalizada(r'Imagens\CancelarLancamento.png')
+        try:
+            x, y = cancelar_lancamento_click
+            mouseClique(x,y, clicks=3, interval=0.1)
             break
-    aguarde = encontrarImagem(r'_internal\Imagens\Aguarde.png') 
-    while type(aguarde) == pyscreeze.Box:
-        aguarde = encontrarImagem(r'_internal\Imagens\Aguarde.png') 
+        except:
+            pass
+    aguarde = encontrarImagemLocalizada(r'Imagens\Aguarde.png') 
+    while type(aguarde) == tuple:
+        aguarde = encontrarImagem(r'Imagens\Aguarde.png') 
         sleep(1)
-    checarFailsafe()
 
     
 def contarItemFracionado(quantidade_siga, valor_unit, quantidade_real):
@@ -209,7 +199,6 @@ def contarItemFracionado(quantidade_siga, valor_unit, quantidade_real):
     sleep(0.2)
     hotkey("ctrl", "c", interval=0.5)
     cod_item = paste()
-    checarFailsafe()
     try:
         while sum(quantidade_total) < quantidade_real:
             press("down")
@@ -225,8 +214,7 @@ def contarItemFracionado(quantidade_siga, valor_unit, quantidade_real):
                 quantidade_total.append(qtd_dividida)
                 press("right")
                 write(valor_unit, interval=0.05)
-                press(["left"]*8)
-                checarFailsafe()        
+                press(["left"]*8)    
             else:
                 break
     except TypeError:
@@ -239,7 +227,6 @@ def contarItemFracionado(quantidade_siga, valor_unit, quantidade_real):
     press(["right"]*7)
     sleep(0.5)
     write(valor_unit, interval=0.05)
-    checarFailsafe()
     try:
         if sum(quantidade_total) != quantidade_real:
             cancelar_lancamento = True
@@ -249,33 +236,28 @@ def contarItemFracionado(quantidade_siga, valor_unit, quantidade_real):
                 razao = qtd / quantidade_real
                 razoes.append(razao)
             press(["right"]*3)
-        checarFailsafe()
     except TypeError:
         cancelar_lancamento = True
         cancelarEMudar()
-        checarFailsafe()
     return razoes, cancelar_lancamento
 
 
 def clicarValorParcela():
-    valor_parcela = encontrarImagemLocalizada(r'_internal\Imagens\clicarParcela.png')
+    valor_parcela = encontrarImagemLocalizada(r'Imagens\clicarParcela.png')
     while type(valor_parcela) != tuple:
         moveTo(180, 200)
-        aba_duplicatas = encontrarImagemLocalizada(r'_internal\Imagens\AbaDuplicatas.png')
+        aba_duplicatas = encontrarImagemLocalizada(r'Imagens\AbaDuplicatas.png')
         x, y =  aba_duplicatas
-        checarFailsafe()
         mouseClique(x,y, clicks=4, interval=0.1)
-        valor_parcela = encontrarImagemLocalizada(r'_internal\Imagens\clicarParcela.png')
+        valor_parcela = encontrarImagemLocalizada(r'Imagens\clicarParcela.png')
         sleep(0.4)
     x, y = valor_parcela
     mouseClique(x,y)
-    checarFailsafe()
 
 
 def clicarNaturezaDuplicata():
     while True:
-        natureza_duplicata_clique = encontrarImagemLocalizada(r'_internal\Imagens\naturezaDuplicata.png')
-        checarFailsafe()
+        natureza_duplicata_clique = encontrarImagemLocalizada(r'Imagens\naturezaDuplicata.png')
         if type(natureza_duplicata_clique) != tuple:
             moveTo(150, 250)
             mouseClique(x,y, clicks=4, interval=0.1)
@@ -284,7 +266,6 @@ def clicarNaturezaDuplicata():
             break
     x, y = natureza_duplicata_clique
     mouseClique(x,y)
-    checarFailsafe()
 
 
 def enviarEmail(corpo):
@@ -362,4 +343,7 @@ def abrirLinkSelenium(lista):
                     break
         except IndexError:
             driver.quit()
+
+
+
 
